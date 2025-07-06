2140558259ca83fa8a3e33d129d75bf492028e90
raffa <zus6wyq9v@mozmail.com>
2025-07-06 05:59:02 +0000

    @fix: refactored keys to public to be accessible to other file. env files are NOT exposed.

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -1,4 +1,4 @@
 export class Environment {
-    private  static SUPABASE_API_URL: string = process.env.SUPABASE_API_URL || "";
-    private static SUPABASE_API_KEY: string = process.env.SUPABASE_API_KEY || "";
+    public static SUPABASE_API_URL: string = process.env.SUPABASE_API_URL || "";
+    public static SUPABASE_API_KEY: string = process.env.SUPABASE_API_KEY || "";
 }
