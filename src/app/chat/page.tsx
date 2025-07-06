8d069296dbdf28ae60c867848c1f2566837a4878
raffa <zus6wyq9v@mozmail.com>
2025-07-06 06:00:34 +0000

    removed .tsx extension to chat/page in its component imports.

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import React, { useState, useEffect } from 'react'
 import { ArrowUp } from "lucide-react";
-import { Button } from '../../components/ui/button.tsx';
-import { Textarea } from '../../components/ui/textarea.tsx';
+import { Button } from '../../components/ui/button';
+import { Textarea } from '../../components/ui/textarea';
 import axios from 'axios';
-import { ScrollArea } from '../../components/ui/scroll-area.tsx';
+import { ScrollArea } from '../../components/ui/scroll-area';
 
 type Message = {
     question: string;
